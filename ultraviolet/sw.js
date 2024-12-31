451e170e240769093643f232817bc289c77e8cf5
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-12-31 11:01:13 -0800

    Update sw.js

--- ultraviolet/sw.js
+++ ultraviolet/sw.js
@@ -2,6 +2,16 @@ importScripts('/epoxy/index.js');
 importScripts('/ultraviolet/uv.bundle.js');
 importScripts('/ultraviolet/uv.config.js');
 importScripts('/ultraviolet/uv.sw.js');
+importScripts('/workerware/index.js');
+const ww = new WorkerWare({
+    debug: true,
+});
+
+ww.use({
+  function: self.adblockExt.filterRequest,
+  events: ["fetch"],
+  name: "Adblock",
+});
 
 const sw = new UVServiceWorker();
 
